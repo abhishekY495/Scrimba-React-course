ccfb8e3d7cdcb150bc3d07535c09936772932096
Abhishek <67749693+abhishake21@users.noreply.github.com>
2022-06-27 23:03:17 +0530

    Added memeState

--- 05-meme-generator/src/components/MemeForm.js
+++ 05-meme-generator/src/components/MemeForm.js
@@ -1,17 +1,23 @@
-import memesData from "../memesData"
+import React from "react";
+import memesData from "../memesData";
 
 export default function MemeForm() {
+    const [memeUrl, setMemeUrl] = React.useState("http://i.imgflip.com/1bij.jpg");
+
     function getMemeImage() {
-        const memesArray = memesData.data.meme;
+        const memesArray = memesData.data.memes;
         const randomNumber = Math.floor(Math.random() * memesArray.length);
-        const url = memesArray[randomNumber].url
+        const memeUrl = memesArray[randomNumber].url;
+        setMemeUrl(memeUrl);
     }
+
     return (
         <main>
             <div className="form">
                 <input type="text" placeholder="Top Text" />
                 <input type="text" placeholder="Bottom Text" />
                 <button onClick={getMemeImage} className="form-button">Get a new meme image 🖼</button>
+                <img src={memeUrl} alt="meme" className="meme-image" />
             </div>
         </main>
     )
