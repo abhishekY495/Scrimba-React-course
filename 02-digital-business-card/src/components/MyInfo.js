13142a586fc8b087279214c0a0a36664601b3d7a
Abhishek Yadav <67749693+abhishekY495@users.noreply.github.com>
2022-11-29 17:54:23 +0530

    Update site

--- 02-digital-business-card/src/components/MyInfo.js
+++ 02-digital-business-card/src/components/MyInfo.js
@@ -6,7 +6,7 @@ export default function MyInfo() {
             <img src={profilePic} className="profile-pic" alt="Me at a beach chilling."/>
             <h2 className="name">Abhishek</h2>
             <p className="role">Frontend Developer</p>
-            <a href="https://abhishake21.com" className="website">abhishake21.com</a>
+            <a href="https://abhisheky495.netlify.app" className="website">My Website</a>
         </div>
     )
 }
